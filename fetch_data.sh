a239683029ba1b2a80428bbeec1c297e5d5d32cc
Hayden Pennington <haydenpennington@icloud.com>
2022-11-12 21:12:23 -0600

    Update fetch to cleanup

--- fetch_data.sh
+++ fetch_data.sh
@@ -4,4 +4,6 @@ if [ ! -d "./videos" ]
 then
     curl -O https://hpennington-cv.s3.amazonaws.com/visual_odometry_videos.zip
     unzip ./visual_odometry_videos
+    rm visual_odometry_videos.zip
+    rm -rf __MACOSX
 fi
