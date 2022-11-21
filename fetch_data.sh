bca183e55d4db8f614abfa0105be26510e7e2e85
Hayden Pennington <haydenpennington@icloud.com>
2022-11-21 14:32:06 -0600

    Split files

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-if [ ! -d "./videos" ]
+if [ ! -d "../videos" ]
 then
+    cd ..
     curl -O https://hpennington-cv.s3.amazonaws.com/visual_odometry_videos.zip
     unzip ./visual_odometry_videos
     rm visual_odometry_videos.zip
-    rm -rf __MACOSX
+    cd src
 fi
