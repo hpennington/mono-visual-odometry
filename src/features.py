ede197ef4df016148562bf39105aa1ddff65986d
Hayden Pennington <haydenpennington@icloud.com>
2022-11-30 06:56:03 -0600

    Move feature methods to features.py

--- src/features.py
+++ src/features.py
@@ -1,5 +1,19 @@
 import cv2
+import numpy as np
 
+## Create the ORB feature detector
+orb = cv2.ORB_create()
+
+## Create the feature matcher
+bf_matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
+
+def compute_orb_descriptors(im, keypoints):
+    return orb.compute(im, keypoints)
+
+def bruteforce_match(descriptors1, descriptors2):
+    matches = bf_matcher.knnMatch(descriptors1, descriptors2, k=2)
+    matches = np.asarray(matches)
+    return matches
 
 def extract_features(im, max_corners, quality, kernel_size, keypoint_size):
     corners = cv2.goodFeaturesToTrack(
