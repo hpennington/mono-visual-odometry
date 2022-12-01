196b4db85562456763b9106972fd3a2161d0e452
Hayden Pennington <haydenpennington@icloud.com>
2022-11-30 22:26:59 -0600

    Clean up more

--- src/features.py
+++ src/features.py
@@ -8,9 +8,6 @@ orb = cv2.ORB_create()
 ## Create the feature matcher
 bf_matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
 
-def compute_orb_descriptors(im, keypoints):
-    return orb.compute(im, keypoints)
-
 def bruteforce_match(descriptors1, descriptors2):
     matches = bf_matcher.knnMatch(descriptors1, descriptors2, k=2)
     matches = np.asarray(matches)
@@ -25,4 +22,5 @@ def extract_features(im, max_corners, quality, kernel_size, keypoint_size):
     )
 
     key_points = [cv2.KeyPoint(x=x[0][0], y=x[0][1], size=keypoint_size) for x in corners]
-    return key_points
+    kps, descriptors = orb.compute(im, key_points)
+    return np.array(kps), descriptors
