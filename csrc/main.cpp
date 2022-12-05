7a53dbff80707c0cd24bc7eda7a29cf0c1a9399a
Hayden Pennington <haydenpennington@icloud.com>
2022-12-05 06:23:17 -0600

    Prepare image for feature detection

--- csrc/main.cpp
+++ csrc/main.cpp
@@ -1,26 +1,46 @@
 #include <iostream>
+#include <Eigen/Dense>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
+#include <opencv2/core/eigen.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
-#include <Eigen/Dense>
 
 #include "./config.h"
+
+cv::Mat transform_image(cv::Mat in, int n_rows, int n_columns)
+{
+    cv::Mat out = cv::Mat(n_rows, n_columns, CV_8UC1);
+    cv::resize(in, out, cv::Size(n_columns, n_rows), cv::INTER_LINEAR);
+    cv::cvtColor(in, out, cv::COLOR_BGR2GRAY, 1);
+    return out;
+}
 
 int main(int argc, char *argv[]) 
 {
     auto cap = cv::VideoCapture(DATA_INPUT);
-    cv::Mat frame;
-
+    cv::Mat cv2_original;
+    
     while (cap.isOpened()) {
-        bool frameGrabbed = cap.read(frame);
+        bool frameGrabbed = cap.read(cv2_original);
         
         if (frameGrabbed == false) {
             break;
         }
 
-        cv::imshow("Frame", frame);
+        // Convert to B&W then resize and convert to Eigen matrix
+        cv::Mat cv2_frame = transform_image(cv2_original, im_h, im_w);
+        // Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic> frame;
+        
+        // std::cout << cv2_frame.channels() << std::endl;
+        // cv::cv2eigen(cv2_frame, frame);
+
+        // Eigen::IOFormat fmt(4, 0, ", ", "\n", "", "");
+
+        // std::cout << frame.format(fmt) << std::endl;
+
+        cv::imshow("Frame", cv2_original);
 
         int keyCode = cv::waitKey(1);
         if (keyCode == 113) {
