b295180034a393355c933f59944834ef3a311e93
Hayden Pennington <haydenpennington@icloud.com>
2022-12-04 21:10:04 -0600

    Get video playing in c++

---
+++ csrc/config.h
@@ -0,0 +1,31 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#include <string>
+
+// Script parameters
+bool KITTI = true;
+const std::string KITTI_DATA_DIRECTORY = "/Users/haydenpennington/dev/data/kitti/";
+const std::string DATA_INPUT = "../../videos/test_countryroad.mp4";
+const int im_w = 640;
+const int im_h = 480;
+const int im_size[2] = {im_w, im_h};
+
+// ORB Detector parameters
+const int max_corners = 1500;
+const int kernel_size = 6;
+const float quality = 0.001;
+const int keypoint_size = 18;
+
+// Ransac parameters
+const int ransac_minsamples = 8;
+const int ransac_max_trials = 150;
+const float ransac_residual_threshold = 0.001;
+
+// Pose extratction translation scaling
+const float tscale = 1.0;
+
+// Point cloud clustering
+const int n_points = 2;
+const float dbscan_eps = tscale * 0.5;
+
+#endif
--- csrc/main.cpp
+++ csrc/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
 #include <Eigen/Dense>
+
+#include "./config.h"
 
 int main(int argc, char *argv[]) 
 {
-    std::cout << "Hello, world!" << std::endl;
+    auto cap = cv::VideoCapture(DATA_INPUT);
+
+
+    cv::Mat frame;
+
+    while (cap.isOpened()) {
+        cap.read(frame);
+
+        cv::imshow("Frame", frame);
+        int keyCode = cv::waitKey(1);
+
+        if (keyCode == 113) {
+            break;
+        }
+    }
+
     return 0;
 }
